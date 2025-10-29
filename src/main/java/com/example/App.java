6346fa2683d7c279a5a6921cae3143726226d1a4
nicksakwa <nsakwa@gmail.com>
2025-10-29 22:31:50 +0300

    pom.xml

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example;
+public class App{
+    public static void main(String[] args){
+        System.out.println("Hello , Maven! This is my first Maven project.");
+        System.out.println("The pom.xml file manages the project dependencies and build configuration.");
+    }
+}
